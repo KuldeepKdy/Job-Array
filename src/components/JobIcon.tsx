80c5090e60597e8bd6534165a226f2f1b79f9895
Kuldeep Yadav <kuldeepkdy4@gmail.com>
2025-02-08 16:59:06 +0530

    recruiter jobs data action done with common card added

---
+++ src/components/JobIcon.tsx
@@ -0,0 +1,12 @@
+import { Laptop } from "lucide-react";
+import { Fragment } from "react";
+
+const JobIcon = () => {
+  return (
+    <Fragment>
+      <Laptop />
+    </Fragment>
+  );
+};
+
+export default JobIcon;
--- src/components/jobs-listing/RecruiterJobCard.tsx
+++ src/components/jobs-listing/RecruiterJobCard.tsx
@@ -1,11 +1,27 @@
+"use client";
+
+import CommonCard from "../CommonCard";
+import JobIcon from "../JobIcon";
+import { Button } from "../ui/button";
+
 interface RecruiterJobCardProps {
   jobItem: { id: string; title: string; description: string };
 }
 
 const RecruiterJobCard = ({ jobItem }: RecruiterJobCardProps) => {
-  return <div>
-    
-  </div>;
+  return (
+    <div>
+      <CommonCard
+        icon={JobIcon}
+        title={jobItem?.title}
+        footerContent={
+          <Button className="flex h-11 items-center justify-center px-5">
+            10 Applicants
+          </Button>
+        }
+      />
+    </div>
+  );
 };
 
 export default RecruiterJobCard;
