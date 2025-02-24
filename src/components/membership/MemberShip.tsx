1198dd633708be3fa84f4df33c1d387128e4eb74
Kuldeep Yadav <kuldeepkdy4@gmail.com>
2025-02-24 23:42:34 +0530

    membership page border issue resolved

--- src/components/membership/MemberShip.tsx
+++ src/components/membership/MemberShip.tsx
@@ -61,7 +61,7 @@ const MemberShip = ({ profileInfo }: membershipInterface) => {
   }
   return (
     <div className="mx-auto max-w-7xl">
-      <div className="flex items-baseline justify-between border border-b pb-6 pt-24">
+      <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
         <h1 className="text-4xl font-bold tracking-tighter text-gray-950">
           Choose Your Best Plan
         </h1>
