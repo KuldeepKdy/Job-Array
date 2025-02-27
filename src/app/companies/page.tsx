034b67de6a455d39e526a578c026b791200f6f3b
Kuldeep Yadav <kuldeepkdy4@gmail.com>
2025-02-27 10:40:44 +0530

    companies page integration on the way

--- src/app/companies/page.tsx
+++ src/app/companies/page.tsx
@@ -1,12 +1,16 @@
-import { fetchProfileAction } from "@/actions";
+import { fetchJobsForCandidateAction, fetchProfileAction } from "@/actions";
 import Companies from "@/components/companies/Companies";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
 
-export const CompaniesPage = async() => {
+const CompaniesPage = async () => {
   const user = await currentUser();
-  const profileInfo = await fetchProfileAction(user?.id)
-  if (!profileInfo) redirect("/onboard") 
-  return <Companies />;
+  const profileInfo = await fetchProfileAction(user?.id);
+  if (!profileInfo) redirect("/onboard");
+  const jobsList = await fetchJobsForCandidateAction({});
+
+  return <Companies jobsList={jobsList} />;
 };
+
+export default CompaniesPage;
--- src/components/companies/Companies.tsx
+++ src/components/companies/Companies.tsx
@@ -1,9 +1,59 @@
-"use client"
+"use client";
 
-const Companies = () => {
-  return (
-    <div>Companies</div>
-  )
+interface CompaniesInterface {
+  jobsList: Array<{
+    companyName: string;
+    title: string;
+    location: string;
+    type: string;
+    experience: string;
+    description: string;
+    skills: string;
+    recruiterId: string;
+    applicants: {
+      name: string;
+      email: string;
+      userId: string;
+      status: string;
+    }[];
+  }>;
 }
+const Companies = ({ jobsList }: CompaniesInterface) => {
+  // console.log(jobsList);
+  const createUniqueSetOfCompanies = [
+    ...new Set(
+      jobsList
+        .filter(
+          (jobItem) =>
+            jobItem?.companyName && jobItem?.companyName.trim() !== ""
+        )
+        .map((item) => item?.companyName)
+    ),
+  ];
+  console.log(createUniqueSetOfCompanies);
+  return (
+    <div className="mx-auto max-w-7xl">
+      <div className="flex items-baseline justify-between border-b pb-6 pt-24">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900">
+          BrowseCompanies
+        </h1>
+      </div>
+      <div className="pt-6 pb-24">
+        <div className="grid grid-cols-1 gap-x-8  gap-y-10 lg:grid-cols-3">
+          <div className="lg:col-span-4">
+            <div className="container mx-auto p-0 space-y-8">
+              <div className="grid grid-cols-1 gap-x-8  gap-y-10 lg:grid-cols-3">
+                {createUniqueSetOfCompanies &&
+                createUniqueSetOfCompanies.length > 0
+                  ? createUniqueSetOfCompanies.map((companyName) => )
+                  : "No Companies Found"}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Companies
+export default Companies;
