9e5e8d579e681e780093a1640559f24833154985
Kuldeep Yadav <kuldeepkdy4@gmail.com>
2025-03-09 23:57:33 +0530

    about section new design on the way

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,18 +1,20 @@
 "use client";
 import { desVariants, tagVariants, titleVariants } from "@/utils/animations";
 import { motion } from "framer-motion";
+import { FlagIcon } from "lucide-react";
 
 const AboutSection = () => {
   return (
     <div className="w-full mt-12 md:mt-0">
-      <div className="grid lg:grid-cols-2 gap-6 md:gap-10 place-items-center">
-        <div className="items-center">
+      <div className="grid lg:grid-cols-2 w-full gap-6 md:gap-10 place-items-center">
+        <div className=" w-full flex flex-col">
           <motion.h2
             initial="offscreen"
             whileInView={"onscreen"}
             variants={titleVariants}
-            className=" text-3xl font-extrabold leading-tight lg:text-5xl"
+            className=" flex gap-1 items-center  text-primary text-xs font-medium"
           >
+            <FlagIcon className="size-3.5 stroke-primary" />
             More about JobArray
           </motion.h2>
           {/* <motion.p
@@ -23,7 +25,7 @@ const AboutSection = () => {
           >
             World Award
           </motion.p> */}
-          <motion.p
+          {/* <motion.p
             initial="offscreen"
             whileInView={"onscreen"}
             variants={tagVariants}
@@ -44,7 +46,34 @@ const AboutSection = () => {
             provide the best interior design for your home. We have been trusted
             by many people to design their homes. We have also received many
             awards from various countries for our work.
-          </motion.p>
+          </motion.p> */}
+          <h2 className="text-2xl text-gray-900 mt-4 font-semibold">
+            Unlock Your True Potential And Discover A World Of Opportunities
+            That Align With Your Skills, Interests, And Aspirations
+          </h2>
+          <div className="flex gap-6 w-full  items-center">
+            <img
+              src="/images/spotify-logo.png"
+              alt="spotify-logo"
+              className="max-w-[20%] max-h-6"
+            />
+
+            <img
+              src="/images/microsoft-logo.png"
+              alt="spotify-logo"
+              className="max-w-[20%] max-w-[20%] max-h-6"
+            />
+            <img
+              src="/images/netflix-logo.png"
+              alt="spotify-logo"
+              className="max-w-[20%] max-w-[20%] max-h-6"
+            />
+            <img
+              src="/images/g-fiber-logo.png"
+              alt="spotify-logo"
+              className="max-w-[20%] max-w-[20%] max-h-6"
+            />
+          </div>
         </div>
         <motion.div
           initial="offscreen"
