7ea4127d8631898e786d10ea9f109e41e0e880f6
Kuldeep Yadav <kuldeepkdy4@gmail.com>
2025-03-22 00:43:11 +0530

    style: center SignIn and SignUp components with flexbox layout

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
-
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="w-full h-full flex items-center justify-center py-8 md:py-10">
+      <SignUp />
+    </div>
+  );
 }
